ab4589d540f277b5b1f4e2b5c3d57d431ef164f5
Istvan Kovacs <istvan.kovacs@mattakis.com>
2016-07-20 12:28:57 +0200

    Removed app module

--- predictsdk/src/androidTest/java/com/emarsys/predict/JSApiSamples.java
+++ predictsdk/src/androidTest/java/com/emarsys/predict/JSApiSamples.java
@@ -14,82 +14,53 @@
  * limitations under the License.
  */
 
-package com.emarsys.predict.app;
-
-import com.emarsys.predict.CartItem;
-import com.emarsys.predict.CompletionHandler;
-import com.emarsys.predict.Error;
-import com.emarsys.predict.ErrorHandler;
-import com.emarsys.predict.RecommendationRequest;
-import com.emarsys.predict.RecommendationResult;
-import com.emarsys.predict.RecommendedItem;
-import com.emarsys.predict.Session;
-import com.emarsys.predict.Storage;
-import com.emarsys.predict.Transaction;
+package com.emarsys.predict;
 
 import junit.framework.Assert;
 
-import android.app.Activity;
-import android.content.SharedPreferences;
-import android.os.Bundle;
-import android.preference.PreferenceManager;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
 import android.support.annotation.NonNull;
+import android.support.test.runner.AndroidJUnit4;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class MainActivity extends Activity {
+import static org.junit.Assert.assertEquals;
 
-    private static final String TAG = MainActivity.class.getSimpleName();
+@RunWith(AndroidJUnit4.class)
+public class JSApiSamples {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    private static final String TAG = JSApiSamples.class.getSimpleName();
 
+    @BeforeClass
+    public static void setUp() {
         // Init the Session
-        Session.initialize(new Storage() {
+        SessionHelper.reset();
+    }
 
-            SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(MainActivity.this);
-
-            @Override
-            public void put(String key, Object value) {
-                SharedPreferences.Editor editor = sharedPref.edit();
-                editor.putString("advertisingId", value.toString());
-                editor.commit();
-            }
-
-            @Override
-            public Object get(String key) {
-                return sharedPref.getString("advertisingId", null);
-            }
-
-        });
-
+    @Before
+    public void setMerchantId() {
         // These parts of our API should be implemented on ALL website pages
         Session session = Session.getInstance();
         // Identifies the merchant account (here the emarsys demo merchant 1A65B5CB868AFF1E).
-        // Replace it with your own Merchant Id before running the application.
+        // Replace it with your own Merchant Id before run.
         session.setMerchantId("1A74F439823D2CB4");
-
-        sample1();
-        sample2();
-        sample3();
-        sample4();
-        sample5();
-        sample6();
-        sample7();
+        assertEquals(session.getMerchantId(), "1A74F439823D2CB4");
+        Session.getInstance().setSecure(true);
     }
 
     /**
      * Port of JavaScript API sample
      * http://jsfiddle.net/0xrprjrz/1/
      */
-    void sample1() {
+    @Test
+    public void sample1() {
         // Identifying the visitor. This email is matched with contact DB.
         // As you can see in LIVE EVENTS, the email is hashed by our API
         Session.getInstance().setCustomerEmail("visitor@test-mail.com");
@@ -114,7 +85,8 @@ public class MainActivity extends Activity {
      * Port of JavaScript API sample
      * http://jsfiddle.net/px15qkk4/
      */
-    void sample2() {
+    @Test
+    public void sample2() {
         // The usual commands to identify visitors and report cart contents.
         Session.getInstance().setCustomerEmail("visitor@test-mail.com");
         Transaction t = new Transaction();
@@ -140,7 +112,8 @@ public class MainActivity extends Activity {
      * Port of JavaScript API sample
      * http://jsfiddle.net/dwoympfa/
      */
-    void sample3() {
+    @Test
+    public void sample3() {
         // The usual commands to identify visitors and report cart contents.
         Session.getInstance().setCustomerEmail("visitor@test-mail.com");
         Transaction t = new Transaction();
@@ -166,7 +139,8 @@ public class MainActivity extends Activity {
      * Port of JavaScript API sample
      * http://jsfiddle.net/m3720jea/
      */
-    void sample4() {
+    @Test
+    public void sample4() {
         // The usual commands to identify visitors and report cart contents.
         Session.getInstance().setCustomerEmail("visitor@test-mail.com");
         Transaction t = new Transaction();
@@ -193,7 +167,8 @@ public class MainActivity extends Activity {
      * Port of JavaScript API sample
      * http://jsfiddle.net/scarabresearch/3Z3UQ/?utm_source=website&utm_medium=embed&utm_campaign=3Z3UQ
      */
-    void sample5() {
+    @Test
+    public void sample5() {
         Transaction t = new Transaction();
         RecommendationRequest recommend = new RecommendationRequest("PERSONAL");
         t.recommend(recommend, new CompletionHandler() {
@@ -221,7 +196,8 @@ public class MainActivity extends Activity {
      * Port of JavaScript API sample
      * http://jsfiddle.net/scarabresearch/82tk48fe/?utm_source=website&utm_medium=embed&utm_campaign=82tk48fe
      */
-    void sample6() {
+    @Test
+    public void sample6() {
         Transaction t = new Transaction();
         t.cart(new ArrayList<CartItem>());
         for (int i = 1; i < 11; i++) {
@@ -254,7 +230,8 @@ public class MainActivity extends Activity {
      * Port of JavaScript API sample
      * http://jsfiddle.net/scarabresearch/7PZCv/?utm_source=website&utm_medium=embed&utm_campaign=7PZCv
      */
-    void sample7() {
+    @Test
+    public void sample7() {
         Transaction t = new Transaction();
         // no filter, just get recs
         t.view("172");
@@ -323,28 +300,6 @@ public class MainActivity extends Activity {
                 Log.d(TAG, error.toString());
             }
         });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
 }
